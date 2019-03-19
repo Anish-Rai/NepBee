10c7edd3680d0f46188cd805c35048c283e25bfc
Anish-Rai <arvik.rai3377@gmail.com>
2019-03-19 15:59:46 +0545

    added responsiveness on navbar

--- js/header.js
+++ js/header.js
@@ -1,10 +1,22 @@
 $(window).on('scroll',function(){
   if($(window).scrollTop()){
-      $('nav').removeClass('container');
-      $('nav').addClass('black');
-  }
+      $('nav').addClass('nav-color');
+      $('.logo-b').css({'visibility':'hidden'});
+      $('.logo').css({'color':'#fff','margin-bottom':'0rem','font-size':'2rem'});
+      $('.container').css({'padding-top':'0','transition':'1s'});
+      $('.b').css({'opacity':'1','transition':'3s'});
+      $('.nav-link').removeClass('nav-wo-s');
+      $('.nav-item').addClass('hvr-w-s');
+
+}
+
   else{
-    $('nav').addClass('container');
-    $('nav').removeClass('black');
+    $('nav').removeClass('nav-color');
+    $('.logo-b').css({'visibility':'visible'});
+      $('.logo').css('color','#FF9900');
+      $('.container').css('padding-top','3rem');
+      $('.b').css({'opacity':'0','transition':'0s'});
+      $('.nav-link').addClass('nav-wo-s');
+      $('.nav-item').removeClass('hvr-w-s');
   }
 })
